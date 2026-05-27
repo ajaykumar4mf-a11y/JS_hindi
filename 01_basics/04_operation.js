40b52ecbb355ba46583e445d6364feb6cb9758ae
Ajay Kumar Yadav <ajaykumar4mf@gmail.com>
2026-05-27 09:12:00 +0000

    operation

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,37 @@
+let value = 3
+let negValue = -value
+
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**2)
+// console.log(3%2)
+
+let str1 = "ajay"
+let str2 = "Kumar"
+let str3 = str1 + str2
+
+// console.log(str3)
+
+
+//------------------------------------------------------------------------------------------------------------
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "4" + 5 + 6)
+// console.log(1 + 2 + "4" + 12 / 6)
+
+
+/*
+Note : 
+ before encountering the the first string all the "+" are considered as arithmetic operation 
+ and after encountering the first string all the "+" are considered as concatenation operator although all the
+ remaining symbol behaves as arithmetic operator
+*/
+
+// console.log(true+)  => throws error bcz + in the last position of parenthesis has no meaning 
+console.log(+true)
+console.log(+"")
