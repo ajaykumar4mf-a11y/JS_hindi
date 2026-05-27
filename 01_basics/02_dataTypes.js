868fa831eb960c1bdadc93ab721319ef92c9fc6b
Ajay Kumar Yadav <ajaykumar4mf@gmail.com>
2026-05-27 07:50:51 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodeJS not browser
+
+let name = "ajay"
+let age = 23
+
+
+//DataTypes
+//number
+//bigint
+//string
+//null => standalone value
+//undefined
+//symbol
+//object
+
+console.log(typeof("ajay"))
+console.log(typeof(null))
+console.log(typeof(undefined))
